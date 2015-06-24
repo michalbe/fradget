8008afedf750ba0f0a8ab0ad64b127cf14f394f0
Michał Budzyński <michal@virtualdesign.pl>
2015-06-24 12:38:47 +0200

    :round_pushpin: append another widget functionality

--- fradget.js
+++ fradget.js
@@ -2,10 +2,12 @@
 'use strict';
 
 var buildProperty = function(obj, propertyName, propertyValue, widget){
+  var _selector;
   Object.defineProperty(obj, propertyName, {
     get: function(){
       return {
         bindTo: function(selector) {
+          _selector = selector;
           this.element = widget.querySelector(selector);
           this.element.innerHTML = propertyValue;
         }
@@ -18,11 +20,16 @@ var Fg = function(name, options){
   var widget = document.querySelector('[data-fg="' + name + '"]');
   var data = {};
   for (var option in options.data) {
-    console.log(option);
     buildProperty(data, option, options.data[option], widget);
   }
 
-  return {
-    data: data
-  };
+  //widget = widget.cloneNode(true);
+  widget.data = data;
+  Object.defineProperty(widget, 'element', {
+    get: function(){
+      return widget.cloneNode(true);
+    }
+  });
+
+  return widget;
 };
